5f43b1a0f219248391a4a048205186e8f4cb6791
Piotr <piotr.ostrowski@aol.com>
2022-04-16 16:28:32 +0200

    add typed-in text

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,18 @@
 /** @jsxImportSource theme-ui */
 import './index.css'
 import 'react-toastify/dist/ReactToastify.css'
-import App from 'pages/App'
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom'
 import { ThemeProvider } from 'theme-ui'
 import { InitializeColorMode } from 'theme-ui'
 import { theme } from './theme'
+import { LandingPage } from 'pages/LandingPage'
 
 ReactDOM.render(
   <StrictMode>
     <InitializeColorMode />
     <ThemeProvider theme={theme}>
-      <App />
+      <LandingPage />
     </ThemeProvider>
   </StrictMode>,
   document.getElementById('root')
