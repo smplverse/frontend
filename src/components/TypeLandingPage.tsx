3cb0313cc277f44aa77ff96ecfc5c959b110e1b3
Piotr <piotr.ostrowski@aol.com>
2022-04-16 21:09:32 +0200

    increase ascii size, go a bit slower, stop cursor at the top for 2 secs

--- src/components/TypeLandingPage.tsx
+++ src/components/TypeLandingPage.tsx
@@ -9,21 +9,22 @@ export const TypeLandingPage = ({ setEntered }: Props) => (
   <Typewriter
     onInit={(typewriter) => {
       typewriter
+        .pauseFor(2000)
         .typeString(`27,000 EXPRESSION PARAMETERS<br />`)
-        .pauseFor(500)
+        .pauseFor(600)
         .typeString(`7,667 MESH VERTICES<br />`)
-        .pauseFor(500)
+        .pauseFor(600)
         .typeString(`200 SKIN TEXTURES<br />`)
-        .pauseFor(500)
+        .pauseFor(600)
         .typeString(`11 TYPES OF EYEWEAR<br />`)
-        .pauseFor(500)
+        .pauseFor(600)
         .typeString(`ONE YOU<br />`)
         .typeString(`<br />`)
         .typeString(`<br />`)
-        .typeString(`<div id="logo" style="font-size: 10px;" />`)
-        .pauseFor(500)
+        .typeString(`<div id="logo" />`)
+        .pauseFor(600)
         .pasteString(
-          `<div style="font-size: 9px">
+          `<div style="font-size: 12px">
    _______________/\\/\\/\\/\\/\\__/\\/\\______/\\/\\__/\\/\\/\\/\\/\\____/\\/\\______________
    <br />
    ____________/\\/\\__________/\\/\\/\\__/\\/\\/\\__/\\/\\____/\\/\\__/\\/\\_______________
@@ -126,6 +127,6 @@ export const TypeLandingPage = ({ setEntered }: Props) => (
         })
         .start()
     }}
-    options={{ cursor: '&#9608;', delay: 35 }}
+    options={{ cursor: '&#9608;', delay: 43 }}
   />
 )
