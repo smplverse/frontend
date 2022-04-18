84d26cea5f2e7e2318ff0b14045c183136027360
Piotr <piotr.ostrowski@aol.com>
2022-04-18 16:04:41 +0200

    change font and color for mint page

--- src/components/MintPageText.tsx
+++ src/components/MintPageText.tsx
@@ -1,44 +1,58 @@
+import styled from '@emotion/styled'
+import { Text } from 'theme-ui'
+
+const Container = styled.div`
+  font-family: 'IBM Plex Mono', monospace;
+  color: #ffffff;
+  display: flex;
+  flex-direction: column;
+  text-justify: center;
+  width: 512px;
+  text-align: justify;
+  margin-top: 50px;
+  & > span {
+    margin-top: 15px;
+  }
+`
+
 export const MintPageText = () => (
-  <>
-    <p className="blocktext">
-      <br />
+  <Container>
+    <Text>
       The smplverse is an nft collection of synthetic facial training data from
       the computational infrastructure of the metaverse.
-      <br />
-      <br />
+    </Text>
+    <Text>
       These pfps were procedurally-generated using smpl—a body mesh used to
       animate avatars in mixed reality from sparse inputs—as the basis for new
       algorithms that will improve face and gesture tracking in virtual
       environments.
-      <br />
-      <br />
+    </Text>
+    <Text>
       As a minter, you submit a face image with your webcam or via upload,
       which a face recognition model uses to find the closest matching image in
       the smplverse collection.
-      <br />
-      <br />
+    </Text>
+    <Text>
       After approving the mint transaction, both images are hashed and written
       to the [token ?].
-      <br />
-      <br />
+    </Text>
+    <Text>
       When the image is submitted, it receives one attribute from the model:
       confidence.
-      <br />
-      <br />
+    </Text>
+    <Text>
       Confidence is a declining measure: as more images are minted, your
       likelihood of receiving a high confidence match decreases.
-      <br />
-      <br />
+    </Text>
+    <Text>
       Conversely, your likelihood of receiving an image with rarer visual
       attributes increases.
-    </p>
-    <p className="blocktext">
-      Note on data collection:
-      <br />
-      <br />
+    </Text>
+    <Text>Note on data collection:</Text>
+    <Text>
       We calculate a 128-dimensional vector from your image and use this
       encoding to compare against the smplverse. no personal data is stored on
       our server.
-    </p>
-  </>
+    </Text>
+  </Container>
 )
