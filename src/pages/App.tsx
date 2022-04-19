4c4a72ee2db56fc5f46dbd4c5d30a4cea17aaf46
Piotr <piotr.ostrowski@aol.com>
2022-04-19 17:01:26 +0200

    drop unused page

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -2,19 +2,13 @@ import { useState } from 'react'
 
 import { LandingPage } from './LandingPage'
 import { MintPage } from './MintPage'
-import { UploadPage } from './UploadPage'
 
 // ideally move all the initial state fetching here
 // and use zustand or redux but cba to use redux here tbf
 export const App = () => {
   const [entered, setEntered] = useState(false)
-  const gotTokens = false
-  const wantToUpload = false
-  // return <UploadPage />
-  if (entered) {
+  if (!entered) {
     return <LandingPage setEntered={setEntered} />
-  } else if (gotTokens && wantToUpload) {
-    return <UploadPage />
   }
   return <MintPage />
 }
--- src/pages/UploadPage.tsx
+++
@@ -1,9 +0,0 @@
-import { CenteredColumn, WebcamCapture } from '../components'
-
-export const UploadPage = () => {
-  return (
-    <CenteredColumn>
-      <WebcamCapture />
-    </CenteredColumn>
-  )
-}
