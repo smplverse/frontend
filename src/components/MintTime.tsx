f1a53d8b29aa3436d1c4fd720ce699102192c1f2
Piotr <piotr.ostrowski@aol.com>
2022-04-22 14:20:30 +0200

    fix date

--- src/components/MintTime.tsx
+++ src/components/MintTime.tsx
@@ -1,7 +1,7 @@
 import { Text } from 'theme-ui'
 
 export const MintTime = () => {
-  const mintingDate = new Date(Date.UTC(2022, 6, 22, 12, 0, 0, 0))
+  const mintingDate = new Date(Date.UTC(2022, 5, 24, 19, 0, 0, 0))
   return (
     <Text mt={4} color={'white'}>
       {mintingDate.toDateString()} {mintingDate.getHours()}:00:00
