1d9e1adb8bbc80c12332ec64709e09a148ef3ee6
Piotr <piotr.ostrowski@aol.com>
2022-04-22 14:18:20 +0200

    add carrets to enter, change pauseFor to 1500

--- src/components/TypeLandingPage.tsx
+++ src/components/TypeLandingPage.tsx
@@ -1,8 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import Typewriter from 'typewriter-effect'
 
-import logoString from '../assets/logo-string'
-
 interface Props {
   setEntered: (arg: boolean) => void
 }
@@ -11,7 +9,8 @@ export const TypeLandingPage = ({ setEntered }: Props) => (
   <Typewriter
     onInit={(typewriter) => {
       typewriter
-        .pauseFor(3000)
+        /*
+        .pauseFor(1500)
         .typeString(`27,000 EXPRESSION PARAMETERS`)
         .pauseFor(1500)
         .typeString(`<br />7,667 MESH VERTICES`)
@@ -30,7 +29,10 @@ export const TypeLandingPage = ({ setEntered }: Props) => (
         .pasteString(logoString, document.getElementById('logo'))
         .typeString(`<br />`)
         .typeString(`<br />`)
-        .typeString(`<span id="enter-button">ENTER</span>`)
+      */
+        .typeString(
+          ` &gt;&gt;&gt; <span id="enter-button">ENTER</span> &lt;&lt;&lt;`
+        )
         .callFunction(() => {
           const div = document.getElementById('enter-button')
           if (!div) return
