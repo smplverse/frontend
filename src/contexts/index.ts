d1cf8835b178fe59f867fe75c79c6f092fedd127
Piotr <piotr.ostrowski@aol.com>
2022-04-24 19:56:23 +0200

    add waiting context to be shared globally

---
+++ src/contexts/index.ts
@@ -0,0 +1,10 @@
+import { createContext, Dispatch, SetStateAction } from 'react'
+
+interface IWaitingContext {
+  isWaiting: boolean
+  setIsWaiting?: Dispatch<SetStateAction<boolean>>
+}
+
+export const WaitingContext = createContext<IWaitingContext>({
+  isWaiting: false,
+})
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
 
+import { WaitingContext } from '../contexts'
 import { LandingPage } from './LandingPage'
 import { MintPage } from './MintPage'
 
-// ideally move all the initial state fetching here
-// and use zustand or redux but cba to use redux here tbf
 export const App = () => {
   const [entered, setEntered] = useState(false)
-  if (entered) {
-    return <LandingPage setEntered={setEntered} />
-  }
-  return <MintPage />
+  const [isWaiting, setIsWaiting] = useState(false)
+  return (
+    <WaitingContext.Provider value={{ isWaiting, setIsWaiting }}>
+      {entered ? <LandingPage setEntered={setEntered} /> : <MintPage />}
+    </WaitingContext.Provider>
+  )
 }
