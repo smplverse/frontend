4d7adf1210d321561d8d594fdb30a1ff5c3018d7
William Wiebe <williamcwiebe@protonmail.ch>
2022-06-13 12:48:40 +0200

    launch time

--- src/components/MintTime.tsx
+++ src/components/MintTime.tsx
@@ -1,7 +1,7 @@
 import { Text } from 'theme-ui'
 
 export const MintTime = () => {
-  const mintingDate = new Date(Date.UTC(2022, 5, 24, 17, 0, 0, 0))
+  const mintingDate = new Date(Date.UTC(2022, 5, 24, 16, 0, 0, 0))
   return (
     <Text mt={4} color={'white'}>
       {mintingDate.toDateString()} {mintingDate.getHours()}:00
