c8afc5a305edcdb92b382e0e838b83b44dfbfe31
William Wiebe <williamcwiebe@protonmail.ch>
2022-07-26 22:40:40 -0400

    fix deployment

--- src/components/SybilMintPage.tsx
+++ src/components/SybilMintPage.tsx
@@ -43,7 +43,7 @@ export const SybilMintPage = () => {
         ) : (
           <SybilImageLoad />
         )}
-        <SybilMintingPanel setMinting={setMinting} />
+        <SybilMintingPanel />
         <SybilMintPageText />
         <SybilFooter />
       </Box>
