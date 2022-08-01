54a6fa6418185f55e3f757b892c95c23d3ecb57e
William Wiebe <williamcwiebe@protonmail.ch>
2022-08-01 18:15:04 -0400

    sold out

--- src/components/SybilMintCount.tsx
+++ src/components/SybilMintCount.tsx
@@ -22,31 +22,7 @@ export const SybilMintCount = () => {
         onMouseEnter={() => setShowPrice(true)}
         onMouseLeave={() => setShowPrice(false)}
       >
-        {showPrice ? (
-          <Text color="text">
-            Ξ
-            {mintPrice || (
-              <Spinner
-                color="white"
-                size="10"
-                strokeWidth={3}
-                sx={{ margin: 'none' }}
-              />
-            )}
-          </Text>
-        ) : (
-          <Text color="text">
-            {numberOfRemaining || (
-              <Spinner
-                color="text"
-                size="10"
-                strokeWidth={3}
-                sx={{ margin: 'none' }}
-              />
-            )}
-            /2222
-          </Text>
-        )}
+        <Text color="text">SOLD OUT</Text>
       </MintCountContainer>
     </div>
   )
